24b0d7548d0c4e74fd656ac0dc4955f1db4b5fb8
Oluwasemilore Badejo <badejo.semilore.7@gmail.com>
2022-09-28 12:22:09 +0100

    update index

--- index.js
+++ index.js
@@ -6,36 +6,7 @@ const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 const slugify = require("slugify");
-////////////////////////////
-//////FILES
 
-// blocking, synchronous
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-
-// const textOut = `this is what we know about the avocado: ${textIn}\ncreated at: ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("file written!");
-
-// non-blocking, async
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   if (err) return console.log("ERROR");
-
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("file written!!!!!!!!!!!");
-//       });
-//     });
-//   });
-// });
-// console.log("will read file");
-
-////////////////////////////
-//////SERVER
 tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8"
